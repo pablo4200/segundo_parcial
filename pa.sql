b9cade0afefaf313d3dcf7efff0918ea18447547
pablo4200 <76073296+pablo4200@users.noreply.github.com>
2021-03-10 12:02:06 -0500

    Update pa.sql

--- pa.sql
+++ pa.sql
@@ -1,16 +1,16 @@
-create or replace function obtener_clasificacion_producto(
-    IN  nombre_instrumento varchar,
-    OUT clasificacion_producto varchar
+create or replace function obtener_cantidad_facturas_empleado(
+    IN apellido_empleado varchar,
+    OUT total_facturas varchar
 )
 as $BODY$
 begin
-    SELECT
-    clasificacion_producto.nombre_clasificacion_producto into clasificacion_producto
-    from producto
-    INNER JOIN public.clasificacion_producto on producto.id_producto = clasificacion_producto.id_clasificacion_producto
-    where nombre_producto = nombre_instrumento;
+    select count(distinct factura_venta.id_factura_venta) into total_facturas
+	from factura_venta
+	inner join empleado on empleado.id_empleado = factura_venta.id_empleado
+	inner join tipo_empleado on tipo_empleado.id_tipo_empleado = empleado.id_tipo_empleado
+	where apellidos_empleado = apellido_empleado;
 end
 $BODY$ 
 language plpgsql;
 
-select * from obtener_datos_hotel('SAXOFÓN ALTO CUSTOM EX')
+select * from obtener_cantidad_facturas_empleado('Akers')
